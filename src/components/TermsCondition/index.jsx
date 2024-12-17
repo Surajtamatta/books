b2bba9bf63bd5caa3d62d1e5698cff77f0ed97c8
Surajtamatta <surajtamatta8379911589@gmail.com>
2024-12-18 00:06:46 +0530

    add terms and conditions

--- src/components/TermsCondition/index.jsx
+++ src/components/TermsCondition/index.jsx
@@ -15,12 +15,12 @@ const TermsConditions = () => {
 
     <div class="space-y-4 text-gray-700">
       <h3 class="sm:text-xl text-base text-[#bb87c9] font-semibold mt-2 sm:mt-6">Privacy Policy </h3>
-      <p className='font-montserrat sm:text-base text-[10px] text-justify '>
+      <p className='font-montserrat sm:text-base text-[12px] text-justify '>
       Your privacy is important to me. When you sign up for my newsletter, I collect your email address to share updates, exclusive previews, behind-the-scenes content, and the occasional giveaway or surprise. I will never share, sell, or distribute your information to third parties. This newsletter is intended for readers 18 and older, as some content may include adult themes or explicit material not suitable for younger audiences. If at any time you no longer wish to receive my emails, you can unsubscribe by clicking the “unsubscribe” link at the bottom of any message. 
       </p>
       
       <h3 class="sm:text-xl text-base text-[#bb87c9] font-semibold mt-2 sm:mt-6">Terms of Service</h3>
-      <p className='font-montserrat sm:text-base text-[10px] text-justify '>
+      <p className='font-montserrat sm:text-base text-[12px] text-justify '>
       By subscribing to my newsletter, you agree to receive regular updates, including announcements about my books, giveaways, upcoming projects, and other related content. This newsletter is intended for readers 18+ due to the possibility of adult themes or explicit material. I reserve the right to make changes to the content, frequency, or format of the newsletter at any time. All content shared is for personal use only and may not be reproduced, distributed, or shared without permission. You can unsubscribe at any time, and your email will be handled in accordance with my privacy policy. Thank you for being part of this community—I’m so happy to have you here!
       </p>
     </div>
