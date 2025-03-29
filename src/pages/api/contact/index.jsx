31b215ac6641aa357f61321a700551f05762b8c9
Surajtamatta <87592868+Surajtamatta@users.noreply.github.com>
2025-03-30 00:00:06 +0530

    Update  index.jsx for contact api

--- src/pages/api/contact/index.jsx
+++ src/pages/api/contact/index.jsx
@@ -5,14 +5,17 @@ import nodemailer from "nodemailer";
 const emailuser = process.env.EMAIL_USER;
 const pass = process.env.EMAIL_PASS;
 
-// console.log("EMAIL_USER:", emailuser);
-// console.log("EMAIL_PASS:", pass);
+let emailSent = false;
 
 export default async function handler(req, res) {
-
   if (req.method !== "POST") {
     return res.status(405).json({ message: "Method Not Allowed" });
   }
+
+  if (emailSent) {
+    return res.status(429).json({ message: "Email already sent. Try again later." });
+  }
+  emailSent = true;
 
   const { fullName, email, message } = req.body;
 
@@ -21,38 +24,36 @@ export default async function handler(req, res) {
   }
 
   try {
-    // Create a transporter
     const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com", // Replace with your SMTP server
+      host: "smtp.gmail.com",
       port: 587,
       secure: false,
       auth: {
         user: emailuser,
-        pass: pass
+        pass: pass,
       },
     });
 
-    // Render the JSX to HTML
     const emailHtml = ReactDOMServer.renderToStaticMarkup(
       <EmailConfirmation name={fullName} email={email} message={message} />
     );
 
-    // Email options
     const mailOptions = {
-      from: email, 
-      replyTo: email, 
-      to: emailuser, // Owner's email address
+      from: email,
+      replyTo: email,
+      to: emailuser,
       subject: `New Message Submitted via balkhabra ${fullName}`,
-      text: message, // Plain text body
-      html: emailHtml, // Rendered HTML body
+      text: message,
+      html: emailHtml,
     };
 
-    // Send email
     await transporter.sendMail(mailOptions);
 
     res.status(200).json({ message: "Message sent successfully!" });
   } catch (error) {
     console.error("Error sending email:", error);
     res.status(500).json({ message: "Failed to send the message." });
+  } finally {
+    emailSent = false;
   }
 }
